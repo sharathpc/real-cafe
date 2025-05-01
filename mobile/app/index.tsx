742c213b6cbf8ba3175ffe4e740457f495c3612d
Sharath Chandra Pyarasani <sharath.pyarasani@realpage.com>
2025-05-01 13:49:29 +0530

    Changes related to Authentication

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,8 @@
-import { StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { useEffect } from "react";
+import { Platform, StyleSheet } from "react-native";
+import * as WebBrowser from 'expo-web-browser';
+import { useGlobalSearchParams } from 'expo-router';
+import axios from 'axios';
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
@@ -7,13 +10,24 @@ import { ExternalLink } from "@/components/ExternalLink";
 import { STRAPI_URL } from "@/constants/Variables";
 
 export default function LoginScreen() {
-  const CALLBACK_URL = 'projectc://vendor';
+  const CALLBACK_URL = Platform.OS === 'ios' ? 'projectc://' : 'com.anonymous.projectc://';
+
+  const globSearchParams: any = useGlobalSearchParams();
+
+  useEffect(() => {
+    if(globSearchParams){
+      axios(`${STRAPI_URL}/api/auth/unified/callback`, {
+        params: globSearchParams,
+      }).then(response => console.log(response.data))
+      WebBrowser.dismissBrowser();
+    }
+  }, [globSearchParams])
+
   return (
     <ThemedView style={styles.container}>
       <ExternalLink href={`${STRAPI_URL}/api/connect/unified?callback=${CALLBACK_URL}`}>
         <ThemedText type="link">Login</ThemedText>
       </ExternalLink>
-      <Link href="/vendor">Vendor</Link>
     </ThemedView>
   );
 }
